45efdab4581122418001888ef709a4aa6161de50
Zhijiang Li <zl256@cornell.edu>
2021-12-02 23:44:51 -0800

    Add 2021 day 01 part 2

--- src/Y2021/Day01/Solution.hs
+++ src/Y2021/Day01/Solution.hs
@@ -5,9 +5,16 @@ import Data.List
 solve :: IO ()
 solve = do
     input <- readFile "./src/Y2021/Day01/input.txt"
-    let testInput1 = read <$> lines input
-    print $ solve1 testInput1
+    let depths = read <$> lines input
+    putStrLn $ "2021.01.1: " ++ (show $ solve1 depths)
+    putStrLn $ "2021.01.2: " ++ (show $ solve2 depths)
 
 
 solve1 :: [Int] -> Int
 solve1 depths = fst $ foldl' (\(count, previousDepth) depth -> if depth > previousDepth then (count + 1, depth) else (count, depth)) (0, head depths) depths
+
+solve2 :: [Int] -> Int
+solve2 depths = count
+    where (count, _, _, _) = foldl' (\(c, d1, d2, d3) d -> if d > d3 then (c + 1, d, d1, d2) else (c, d, d1, d2))
+                                    (0, head $ tail $ tail depths, head $ tail depths, head depths)
+                                    (tail $ tail $ tail depths)
