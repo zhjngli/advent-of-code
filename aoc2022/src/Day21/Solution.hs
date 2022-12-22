8319929dc6f432481eb96a175a921b41bb5edb6f
Zhijiang Li <zl256@cornell.edu>
2022-12-22 00:31:41 -0800

    2022 day 21 part 2

--- aoc2022/src/Day21/Solution.hs
+++ aoc2022/src/Day21/Solution.hs
@@ -8,10 +8,11 @@ solve :: IO ()
 solve = do
     input <- readFile "./src/Day21/input.txt"
     putStrLn $ "2022.21.1: " ++ show (solve1 $ parseInput input)
-    -- putStrLn $ "2022.21.2: " ++ show (solve2 $ parseInput input)
+    putStrLn $ "2022.21.2: " ++ show (solve2 $ parseInput input)
 
 data Op = Add | Mul | Sub | Div deriving Show
 data Monkey = M Op (String, String) | I Integer deriving Show
+data MonkeyTree = Root MonkeyTree MonkeyTree | Node Op MonkeyTree MonkeyTree | Leaf Integer | Var deriving Show
 
 parseLeaf :: CharParser st Monkey
 parseLeaf = do
@@ -44,10 +45,6 @@ parseMonkeyLine = do
 parseMonkeys :: CharParser st [(String, Monkey)]
 parseMonkeys = endBy1 parseMonkeyLine newline
 
-monkeyMap :: [(String, Monkey)] -> M.Map String Monkey
-monkeyMap = foldr f M.empty
-    where f (n, m) ms = M.insert n m ms
-
 parseInput :: String -> [(String, Monkey)]
 parseInput i = case parse parseMonkeys "" i of
     Left e -> error (show e)
@@ -73,3 +70,49 @@ calculateMonkey ((n, M o (x, y)):ms) mm =
 
 solve1 :: [(String, Monkey)] -> Maybe Monkey
 solve1 ms = M.lookup "root" $ calculateMonkey ms M.empty
+
+getMonkeyTreeNode :: M.Map String MonkeyTree -> String -> MonkeyTree
+getMonkeyTreeNode mm s = case M.lookup s mm of
+    Just m -> m
+    Nothing -> error (s ++ " should exist in monkey tree map already")
+
+-- this is pretty hacky. what I should do is write new parsing functions and types to create the tree data structure.
+-- but I'm a little lazy to write all that stuff after already writing it a certain way for part 1.
+calculateMonkeyTree :: [(String, Monkey)] -> M.Map String MonkeyTree -> M.Map String MonkeyTree
+calculateMonkeyTree [] mm = mm
+calculateMonkeyTree ((n, I i):ms) mm =
+    if n == "humn" then calculateMonkeyTree ms (M.insert n Var mm)
+    else calculateMonkeyTree ms (M.insert n (Leaf i) mm)
+calculateMonkeyTree ((n, M o (x, y)):ms) mm =
+    if M.member x mm && M.member y mm then
+        if n == "root" then calculateMonkeyTree ms (M.insert n (Root (getMonkeyTreeNode mm x) (getMonkeyTreeNode mm y)) mm)
+        else (let mt = case (getMonkeyTreeNode mm x, getMonkeyTreeNode mm y) of
+                        (Leaf i, Leaf j) -> Leaf (mOp o i j)
+                        (mx, my) -> Node o mx my
+            in calculateMonkeyTree ms (M.insert n mt mm))
+    else calculateMonkeyTree (ms ++ [(n, M o (x, y))]) mm
+
+reverseRootToHumn :: MonkeyTree -> Integer
+reverseRootToHumn (Root (Leaf i) (Node o x y)) = reverseRootToHumn' (Node o x y) i
+reverseRootToHumn (Root (Node o x y) (Leaf i)) = reverseRootToHumn' (Node o x y) i
+reverseRootToHumn mt = error ("function should start from properly formed root, not any other node: " ++ show mt)
+
+reverseRootToHumn' :: MonkeyTree -> Integer -> Integer
+reverseRootToHumn' Var i = i
+reverseRootToHumn' (Node o (Leaf l) x) i = case o of
+    Add -> reverseRootToHumn' x (i - l)
+    Mul -> reverseRootToHumn' x (i `div` l)
+    Sub -> reverseRootToHumn' x (l - i)
+    Div -> reverseRootToHumn' x (l `div` i)
+reverseRootToHumn' (Node o x (Leaf l)) i = case o of
+    Add -> reverseRootToHumn' x (i - l)
+    Mul -> reverseRootToHumn' x (i `div` l)
+    Sub -> reverseRootToHumn' x (i + l)
+    Div -> reverseRootToHumn' x (i * l)
+reverseRootToHumn' x y = error ("cannot reverse tree from: " ++ show x ++ ", " ++ show y)
+
+solve2 :: [(String, Monkey)] -> Integer
+solve2 ms = reverseRootToHumn mt
+    where mt = case M.lookup "root" (calculateMonkeyTree ms M.empty) of
+                Just r -> r
+                Nothing -> error "could not find root"
