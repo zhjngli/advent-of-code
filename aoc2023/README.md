8c81cde1ae307b7c6f3f6b56af2b307a7900414d
Zhijiang Li <zl256@cornell.edu>
2023-12-01 00:19:29 -0800

    advent of code 2023 init

---
+++ aoc2023/README.md
@@ -0,0 +1,5 @@
+# aoc2023
+
+1. `stack build`
+1. `stack exec aoc`
+
---
+++ aoc2023/src/Day01/Solution.hs
@@ -0,0 +1,7 @@
+module Day01.Solution (solve) where
+
+
+solve :: IO ()
+solve = do
+    input <- readFile "./src/Day01/input.txt"
+    putStrLn "not implemented yet!"
