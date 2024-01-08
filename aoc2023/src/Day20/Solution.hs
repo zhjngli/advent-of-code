48441209ee22f725a332d075e87729174c17b6e2
Zhijiang Li <zl256@cornell.edu>
2024-01-08 01:25:17 -0800

    2023 day 20 part 2 messy

--- aoc2023/src/Day20/Solution.hs
+++ aoc2023/src/Day20/Solution.hs
@@ -2,13 +2,25 @@
 module Day20.Solution (solve) where
 
 import Text.ParserCombinators.Parsec
+    ( char,
+      letter,
+      newline,
+      string,
+      endBy1,
+      many1,
+      sepBy1,
+      (<|>),
+      parse,
+      try,
+      CharParser )
 import qualified Data.Map.Strict as M
+import Control.Monad.State.Strict
 
 solve :: IO ()
 solve = do
     input <- readFile "./src/Day20/input.txt"
     putStrLn $ "2023.20.1: " ++ show (solve1 $ parseInput input)
-    -- putStrLn $ "2023.20.2: " ++ show (solve2 $ parseInput input)
+    putStrLn $ "2023.20.2: " ++ show (solve2 $ parseInput input)
 
 data Signal = H | L deriving (Show, Eq)
 data Module = B [String] | F Bool [String] | C (M.Map String Signal) [String] deriving (Show, Eq)
@@ -101,3 +113,100 @@ pulse ms ((from, L, name):ps) h l =
 solve1 :: M.Map String Module -> Int
 solve1 ms = h * l
     where (h, l, _) = iterate pressButton (0, 0, ms) !! 1000
+
+data ModuleState = ModuleState {
+    modules :: M.Map String Module,
+    buttonPresses :: Int,
+    pulses :: [(String, Signal, String)],
+    conIntoRx :: String,
+    modsIntoCon :: M.Map String Int
+} deriving Show
+
+foundCycle :: State ModuleState Bool
+foundCycle = get >>= \ms -> pure (all (> 0) (M.elems $ modsIntoCon ms))
+    -- do
+    -- ms <- get
+    -- return $ all (> 0) (M.elems $ modsIntoCon ms)
+
+-- some copy/pasta cause i'm too lazy to refactor this properly
+singlePulse :: M.Map String Module -> [(String, Signal, String)] -> ([(String, Signal, String)], M.Map String Module)
+singlePulse ms [] = ([], ms)
+singlePulse ms ((from, H, name):ps) =
+    case ms M.!? name of
+    Just (B outs) -> (ps ++ map (name, H,) outs, ms)
+    Just (F _ _) -> (ps, ms)
+    Just (C ins outs) ->
+        let ins' = M.insert from H ins
+            ms' = M.insert name (C ins' outs) ms
+            s = if all (== H) (M.elems ins') then L else H
+        in
+        (ps ++ map (name, s,) outs, ms')
+    Nothing -> (ps, ms)
+singlePulse ms ((from, L, name):ps) =
+    case ms M.!? name of
+    Just (B outs) -> (ps ++ map (name, L,) outs, ms)
+    Just (F True outs) -> (ps ++ map (name, L,) outs, M.insert name (F False outs) ms)
+    Just (F False outs) -> (ps ++ map (name, H,) outs, M.insert name (F True outs) ms)
+    Just (C ins outs) ->
+        let ins' = M.insert from L ins
+            ms' = M.insert name (C ins' outs) ms
+            s = if all (== H) (M.elems ins') then L else H
+        in
+        (ps ++ map (name, s,) outs, ms')
+    Nothing -> (ps, ms)
+
+stPressButton :: State ModuleState ()
+stPressButton = do
+    ms <- get
+    let ps = pulses ms
+    let con = conIntoRx ms
+    let n = if null ps then buttonPresses ms + 1 else buttonPresses ms
+    let (ps', ms') =
+            if null ps then singlePulse (modules ms) [("button", L, "broadcaster")]
+            else singlePulse (modules ms) ps
+    let ins = case ms' M.! con of
+            C i _ -> i
+            _ -> error (show con ++ " should be a cons module with stored inputs")
+    put ms {
+        modules = ms',
+        pulses = ps',
+        buttonPresses = n,
+        modsIntoCon = M.foldlWithKey'
+            (\acc name sig ->
+                -- trace ("in: " ++ name ++ ", sig: " ++ show sig) $
+                if sig == H && acc M.! name == 0 then M.insert name n acc
+                else acc
+            )
+            (modsIntoCon ms) ins
+    }
+    return ()
+
+search :: ModuleState -> Int
+search ms
+    | evalState foundCycle ms = M.foldl' lcm 1 (modsIntoCon ms)
+    | otherwise = search $ execState stPressButton ms
+
+solve2 :: M.Map String Module -> Int
+solve2 ms = search initState
+    where
+        con =
+            let candidates = M.foldlWithKey'
+                    (\acc name m ->
+                        case m of
+                        C _ outs -> if "rx" `elem` outs then name:acc else acc
+                        _ -> acc
+                    )
+                    [] ms
+            in
+            case candidates of
+            [c] -> c
+            _ -> error ("too many candidates for cons modules into rx: " ++ show candidates)
+        initState = ModuleState {
+            modules = ms,
+            buttonPresses = 0,
+            pulses = [],
+            conIntoRx = con,
+            modsIntoCon = case ms M.! con of
+                C ins _ -> M.foldlWithKey' (\acc name _ -> M.insert name 0 acc) M.empty ins
+                _ -> error ""
+        }
