631bdeb24d4470b34e8e7727c3038033454a900c
Zhijiang Li <zl256@cornell.edu>
2024-12-05 15:47:41 -0800

    clean up 2024 day 01

--- aoc2024/src/day01.rs
+++ aoc2024/src/day01.rs
@@ -1,32 +1,18 @@
 use std::fs;
 
-fn parse() -> (Vec<i64>, Vec<i64>) {
-    let mut ls = Vec::new();
-    let mut rs = Vec::new();
-    let result = fs::read_to_string("data/01.txt");
-    result
-        .unwrap_or_else(|error| {
-            eprintln!("Error: {}", error);
-            "1 1\n".to_string()
-        })
-        .lines()
-        .for_each(|line| {
-            let nums: Vec<i64> = line
+fn parse(s: &str) -> (Vec<i64>, Vec<i64>) {
+    s.lines()
+        .map(|line| {
+            let mut nums = line
                 .split_whitespace()
-                .filter_map(|num| num.parse::<i64>().ok())
-                .collect();
-            if nums.len() == 2 {
-                ls.push(nums[0]);
-                rs.push(nums[1]);
-            } else {
-                eprintln!("Skipping line: {}", line);
-            }
-        });
-    (ls, rs)
+                .filter_map(|num| num.parse::<i64>().ok());
+            (nums.next().unwrap(), nums.next().unwrap())
+        })
+        .unzip()
 }
 
-fn solve1() -> i64 {
-    let (mut ls, mut rs) = parse();
+fn solve1(s: &str) -> i64 {
+    let (mut ls, mut rs) = parse(s);
     ls.sort();
     rs.sort();
 
@@ -36,14 +22,21 @@ fn solve1() -> i64 {
         .sum::<u64>() as i64
 }
 
-fn solve2() -> i64 {
-    let (ls, rs) = parse();
+fn solve2(s: &str) -> i64 {
+    let (ls, rs) = parse(s);
     ls.into_iter()
         .map(|l| rs.iter().filter(|&&r| r == l).count() as i64 * l)
         .sum()
 }
 
 pub fn solve() {
-    println!("2024.01.1: {}", solve1());
-    println!("2024.01.2: {}", solve2());
+    let result = fs::read_to_string("data/01.txt");
+    let binding = result.unwrap_or_else(|error| {
+        eprintln!("Error: {}", error);
+        "1 1\n".to_string()
+    });
+    let input = binding.as_str();
+
+    println!("2024.01.1: {}", solve1(input));
+    println!("2024.01.2: {}", solve2(input));
 }
