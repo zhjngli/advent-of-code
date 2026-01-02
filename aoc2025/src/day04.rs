86dec7e02a03203ae5a4e2176b95ff55ac3c19cb
Zhijiang Li <zl256@cornell.edu>
2026-01-01 22:35:39 -0800

    aoc2025 day 04

---
+++ aoc2025/src/day04.rs
@@ -0,0 +1,116 @@
+use std::{collections::HashSet, fs};
+
+fn parse(s: &str) -> Vec<Vec<char>> {
+    s.lines().map(|line| line.chars().collect()).collect()
+}
+
+fn solve1(s: &Vec<Vec<char>>) -> usize {
+    let rows = s.len();
+    let cols = s[0].len();
+    s.iter()
+        .enumerate()
+        .map(|(r, row)| {
+            row.iter()
+                .enumerate()
+                .map(|(c, ch)| {
+                    if ch != &'@' {
+                        0
+                    } else {
+                        let adjacent_rolls = [
+                            (-1, -1),
+                            (-1, 0),
+                            (-1, 1),
+                            (0, -1),
+                            (0, 1),
+                            (1, -1),
+                            (1, 0),
+                            (1, 1),
+                        ]
+                        .iter()
+                        .filter_map(|(dr, dc)| {
+                            let new_r = r as isize + dr;
+                            let new_c = c as isize + dc;
+                            if new_r >= 0
+                                && new_r < rows as isize
+                                && new_c >= 0
+                                && new_c < cols as isize
+                                && s[new_r as usize][new_c as usize] == '@'
+                            {
+                                Some(s[new_r as usize][new_c as usize])
+                            } else {
+                                None
+                            }
+                        })
+                        .count();
+                        if adjacent_rolls < 4 {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                })
+                .sum::<usize>()
+        })
+        .sum::<usize>()
+}
+
+fn solve2(s: &Vec<Vec<char>>) -> usize {
+    let rows = s.len();
+    let cols = s[0].len();
+    let mut new_rolls = true;
+    let mut moved_rolls = HashSet::new();
+    while new_rolls {
+        new_rolls = false;
+        for (r, row) in s.iter().enumerate() {
+            for (c, ch) in row.iter().enumerate() {
+                if ch != &'@' || moved_rolls.get(&(r, c)).is_some() {
+                    continue;
+                }
+                let adjacent_rolls = [
+                    (-1, -1),
+                    (-1, 0),
+                    (-1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
+                ]
+                .iter()
+                .filter_map(|(dr, dc)| {
+                    let new_r = r as isize + dr;
+                    let new_c = c as isize + dc;
+                    if new_r >= 0
+                        && new_r < rows as isize
+                        && new_c >= 0
+                        && new_c < cols as isize
+                        && moved_rolls.get(&(new_r as usize, new_c as usize)).is_none()
+                        && s[new_r as usize][new_c as usize] == '@'
+                    {
+                        Some((new_r, new_c))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<(isize, isize)>>();
+                if adjacent_rolls.len() < 4 {
+                    moved_rolls.insert((r, c));
+                    new_rolls = true;
+                }
+            }
+        }
+    }
+    moved_rolls.len()
+}
+
+pub fn solve() {
+    let result = fs::read_to_string("data/04.txt");
+    let binding = result.unwrap_or_else(|error| {
+        eprintln!("ERROR: {}", error);
+        "\n".to_string()
+    });
+    let input = parse(binding.as_str());
+
+    println!("2025.04.1: {}", solve1(&input));
+    println!("2025.04.2: {}", solve2(&input));
+}
--- aoc2025/src/main.rs
+++ aoc2025/src/main.rs
@@ -2,8 +2,10 @@ mod common;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
+    day04::solve();
     day03::solve();
     day02::solve();
     day01::solve();
